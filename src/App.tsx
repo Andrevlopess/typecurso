53a104bdca246a7c9194d0ca1952bcc40c5147ac
André Lopes <andrellopes021@gmail.com>
2023-01-02 17:40:58 -0300

    aonde está esse commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,65 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  //* the param after the function paramters is the return typeOf
+  function soma(n1: number, n2: number): number {
+    return n1 + n2;
+  }
+
+  //* union type  =  two types
+
+  function exemplo(ex: string | number) {
+    return ex
+  }
+
+  exemplo(100)
+
+  // * type alias -- não pode ser alterado
+
+  type ID = string | number
+
+  function showId(id: ID) {
+    console.log(`O id é ${id}`);
+
+  }
+
+  // * interface -- pode ser alterado a qlqr momento
+
+  interface Point {
+    x: number
+    y: number
+    z: number
+  }
+
+  function showPoint(obj: Point) {
+    return obj
+  }
+
+  const pointObj: Point = {
+    x: 1,
+    y: 2,
+    z: 3 
+  }
+
+  showPoint(pointObj)
+
+  //* literal types -- aceita somente os valores pré determinados
+
+  function showDirection(direction: 'left' | 'right' | 'center'){
+    return direction
+  }
+ 
+  showDirection('left')
+  
+  // * non null assertions operator
+  // valida se o valor pode ser null ou não
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
