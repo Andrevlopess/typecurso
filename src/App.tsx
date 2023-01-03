4f09c8b5e4bf611b3cbaf8bd9443985056501307
André Lopes <andrellopes021@gmail.com>
2023-01-03 14:33:03 -0300

    aula 60

--- src/App.tsx
+++ src/App.tsx
@@ -167,7 +167,54 @@ function App() {
 
     preGreeting(greeting, 'andre')
 
+
+    // * generic functions
+
+    function firstElement<T>(arr: T[]) : T {
+      return arr[0]
+    }
+
+    console.log(firstElement([1,2,3]));
     
+    function mergeObjects<U, T>(obj1: U, obj2: T){
+      return{
+        ...obj1,
+        ...obj2,
+      }
+    }
+
+    const newObject = mergeObjects({name: "andre"}, {age: 15, lastName: "lopes"})
+
+     // console.log(newObject);
+    
+     // * CONSTRAINTS em generic types
+
+     function  biggestNumber<T extends number | string>(a: T, b: T): T {
+
+      let biggest : T
+
+      if(+a > +b){
+        biggest = a
+      }else{
+        biggest = b
+      }
+
+      return biggest
+     }
+
+     console.log(biggestNumber("5", "99"));
+     
+     // * specifying the argument type
+
+     function mergeArrays<T>(arr1 : T[], arr2 : T[]) {
+
+      return arr1.concat(arr2)
+     }
+
+     console.log(mergeArrays([1,2,3], [5,6,7]));
+     console.log(mergeArrays<number | string>([1,2,3], ["andre", 'vitor']));
+     
+
  
   return (
 
